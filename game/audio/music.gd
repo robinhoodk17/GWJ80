ea7bc4b7fe10364a7fd8f2f694eea91cf6d2fab3
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-04-17 05:53:43 +0200

    added the music

---
+++ game/audio/music.gd
@@ -0,0 +1,24 @@
+extends AudioStreamPlayer
+
+@export var next_player : AudioStreamPlayer
+@export var first_minute : bool = false
+
+func _ready() -> void:
+	finished.connect(next_player.play)
+	Globals.restart.connect(on_restart)
+	if first_minute:
+		play()
+
+
+func transition(current_player : AudioStreamPlayer) -> void:
+	var play_position : float = current_player.get_playback_position()
+	current_player.stop()
+	play(play_position)
+
+
+func on_restart() -> void:
+	if !first_minute:
+		stop()
+		return
+
+	play()
