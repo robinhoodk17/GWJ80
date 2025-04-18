4647cffcca49317f53a7c87f9e67be18f0821090
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-04-18 10:23:32 +0200

    implemented the logic for mama bear

--- game/npc/bear/bear_npc.gd
+++ game/npc/bear/bear_npc.gd
@@ -1,14 +1,40 @@
 extends NPC
 
-var already_talked = false
+var quest_finished : bool = false
+var quest_started : bool = false
+@export var barry : NPC
 
 ###Override this function to handle dialogue logic###
 func handle_dialogue_start(_player_controller) -> void:
-	if already_talked:
-		start_dialogue("second_talk")
-	else :
-		start_dialogue("first_talk")
+	if _player_controller.grabbing != null:
+		if _player_controller.grabbing.name == "badge":
+			if Globals.current_time > 360.0 and Globals.current_time < 480.0:
+				start_dialogue("mama_bear_mee_badge")
+				Globals.quest_finished("mee", gamestate.SABOTAGED, -1)
+				return
 
-###Override this function to handle what happens after the dialogue ###
+			if Globals.current_time < 360.0:
+				start_dialogue("mama_bear_too_early")
+				return
+			
+			if Globals.current_time > 480.0:
+				start_dialogue("mama_bear_no_overtime")
+				return
+
+	if !quest_finished:
+		if !quest_started:
+			Globals.quest_started("mama_bear", gamestate.HELPED)
+			quest_started = true
+		start_dialogue("mama_bear_decision_tree")
+		return
+
+	if quest_finished:
+		start_dialogue("mama_bear_correct_choices")
+		return
+
+	if Globals.quest_status["barry"] == "started":
+		start_dialogue("mama_bear_barry_idea")
+		return
+
 func  handle_dialogue_end() -> void:
-	current_gamestate = gamestate.SABOTAGED
+	pass
