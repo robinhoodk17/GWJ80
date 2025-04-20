13080598d3fce0c7c38189e81932baaf109c6269
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-04-20 15:03:44 +0200

    finished with the routes

--- game/scenes/Bigger building made with csg boxeszip/game_jam_sky_scraper.gd
+++ game/scenes/Bigger building made with csg boxeszip/game_jam_sky_scraper.gd
@@ -7,11 +7,13 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	GUIDE.enable_mapping_context(default_mapping_context)
 	ui.get_node("PauseMenu").pausable = true
+	get_tree().paused = false
+	await get_tree().process_frame
+	get_tree().paused = true
 	LoadingScreen.start_animation()
 	await LoadingScreen.finished_loading
 	get_tree().paused = false
 	$Music/ButterflyLoop.play()
 	$Music/ClockSpawn.play()
-	#await get_tree().process_frame
 	$UI.go_to("Game")
 	$UI/Game/Label.start_counting = true
