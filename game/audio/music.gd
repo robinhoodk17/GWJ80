4d3196bcfba933a375d11893053b3e755ef54773
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-04-21 07:10:55 +0200

    rebasing

--- game/audio/music.gd
+++ game/audio/music.gd
@@ -1,15 +1,13 @@
 extends AudioStreamPlayer
 
 @export var next_player : AudioStreamPlayer
+@export var first_minute : bool = false
 
 func _ready() -> void:
-	finished.connect(play_next)
+	finished.connect(next_player.play)
 	Globals.restart.connect(on_restart)
-
-
-func play_next() -> void:
-	if next_player != null:
-		next_player.play()
+	if first_minute:
+		play()
 
 
 func transition(current_player : AudioStreamPlayer) -> void:
@@ -19,4 +17,8 @@ func transition(current_player : AudioStreamPlayer) -> void:
 
 
 func on_restart() -> void:
+	if !first_minute:
+		stop()
+		return
+
 	play()
